0da59d479c7e61f8a1933cb7c3f79cf6213324e1
kokora <liuhongli2120@gmail.com>
2018-11-12 21:51:21 +0800

    11111

---
+++ GaoDeMap/GaoDeMap/GaoDeMap-Bridging-Header.h
@@ -0,0 +1,25 @@
+//
+//  GaoDeMap-Bridging-Header.h
+//  GaoDeMap
+//
+//  Created by 刘宏立 on 2018/11/11.
+//  Copyright © 2018 lhl. All rights reserved.
+//
+
+#ifndef GaoDeMap_Bridging_Header_h
+#define GaoDeMap_Bridging_Header_h
+
+// 高德地图
+//#import <AMapFoundationKit/AMapFoundationKit.h>
+//#import <MAMapKit/MAMapKit.h>
+
+/// 高德地图聚合点
+//#import "CoordinateQuadTree.h"
+//#import "ClusterAnnotation.h"
+//#import "ClusterAnnotationView.h"
+//#import "ClusterTableViewCell.h"
+//#import "CustomCalloutView.h"
+//#import "SWAMapPOI.h"
+
+
+#endif /* GaoDeMap_Bridging_Header_h */
